361130b8867d5e70c6cda6fd68afade42bcace84
jyotsnakeerti <jyotsnakeerti4@gmail.com>
2023-12-09 17:11:57 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// App.js
+
 import React, { Suspense, useState } from 'react';
 import { Canvas } from 'react-three-fiber';
 import { OrbitControls } from '@react-three/drei';
--- src/Lamp.js
+++ src/Lamp.js
@@ -4,29 +4,26 @@ import * as THREE from 'three';
 
 const Lamp = ({ isOn, brightness, color }) => {
   const lampRef = useRef();
-  const standRef = useRef(); // Reference for the lamp stand
+  const standRef = useRef(); 
   const { viewport } = useThree();
 
-  // Adjust sphere size based on viewport size
-  const sphereSize = Math.min(viewport.width, viewport.height) * 0.07; // Reduced the overall size
-  const standHeight = sphereSize * 9; // Adjusted the stand height based on the sphere size
-  const ceilingHeight = 7;
 
-  // Update material properties based on brightness and color
+  const sphereSize = Math.min(viewport.width, viewport.height) * 0.07; 
+  const standHeight = sphereSize * 9;
+  const ceilingHeight = 7;
   const materialProps = {
-    emissive: isOn ? color : 'gray', // Use gray when the lamp is off
+    emissive: isOn ? color : 'gray', 
     emissiveIntensity: brightness,
-    color: isOn ? new THREE.Color(color) : new THREE.Color('gray'), // Set the color when the lamp is on
+    color: isOn ? new THREE.Color(color) : new THREE.Color('gray'),
   };
 
-  // Update sphere size and positions on window resize
+
   useEffect(() => {
     lampRef.current.scale.set(sphereSize, sphereSize, sphereSize);
     standRef.current.position.y = ceilingHeight - standHeight / 2;
   }, [sphereSize]);
 
   useFrame(() => {
-    // Rotate the lamp slowly
     if (lampRef.current) {
       lampRef.current.rotation.x += 0.01;
       lampRef.current.rotation.y += 0.01;
@@ -35,15 +32,14 @@ const Lamp = ({ isOn, brightness, color }) => {
 
   return (
     <group>
-      {/* Circular Plate Ceiling */}
 
-      {/* Lamp Stand */}
+
       <mesh ref={standRef} position={[0, ceilingHeight - standHeight / 2, 0]}>
         <cylinderGeometry args={[0.080, 0.080, standHeight, 32]} />
         <meshStandardMaterial color="silver" />
       </mesh>
 
-      {/* Lamp Base */}
+
       <mesh ref={lampRef} position={[0, ceilingHeight - standHeight, 0]} castShadow>
         <sphereGeometry args={[2.5, 64, 64]} />
         <meshStandardMaterial {...materialProps} />
